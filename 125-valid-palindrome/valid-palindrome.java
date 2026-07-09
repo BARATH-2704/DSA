1df82cb088f3c42235cadb536d800d4316ddbd63
BARATH S <143437323+BARATH-2704@users.noreply.github.com>
2026-07-09 20:21:08 +0530

    Time: 15 ms (20.62%) | Memory: 46.5 MB (20.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.replaceAll("[^a-zA-Z0-9]","");
-        s=s.toLowerCase();
-        int slow=0;
-        for(int fast=s.length()-1;fast>=0;fast--){
-            if(s.charAt(slow)==s.charAt(fast)){
-                slow++;
-            }
-            else{
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        s = s.replace(" ", "");
+        int slow =0;
+        for(int fast =s.length()-1;fast>=slow;fast--){
+            char chf = s.charAt(fast);
+            char chs = s.charAt(slow);
+            if(chs!=chf){
                 return false;
             }
+            slow++;
         }
-        if(slow==s.length()){
-            return true;
-        }
-        else return false;
+        return true;
     }
 }
