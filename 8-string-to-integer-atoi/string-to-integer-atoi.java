f401c74239e3ba2bcd88b225ea1e1cbcd4e349b4
BARATH S <143437323+BARATH-2704@users.noreply.github.com>
2026-07-24 21:06:20 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (41.62%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        int index = 0;
+        int n = s.length();
+
+        while (index < n && s.charAt(index) == ' ') {
+            index++;
+        }
+
+        if (index == n) {
+            return 0;
+        }
+
+        int sign = 1;
+        char firstChar = s.charAt(index);
+        if (firstChar == '+') {
+            index++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            index++;
+        }
+
+        int result = 0;
+        while (index < n) {
+            char currentChar = s.charAt(index);
+            if (currentChar < '0' || currentChar > '9') {
+                break;
+            }
+
+            int digit = currentChar - '0';
+
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            index++;
+        }
+
+        return result * sign;
+    }
+}
